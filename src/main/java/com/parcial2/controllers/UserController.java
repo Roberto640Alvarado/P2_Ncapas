b6f651b75d1957d3f57f682ec6e7514b4e27d44f
EA03 <00175420@uca.edu.sv>
2023-05-28 22:11:58 -0600

    :white_check_mark:Add:search and delete songs by id, delete users by id.

--- src/main/java/com/parcial2/controllers/UserController.java
+++ src/main/java/com/parcial2/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,21 @@ public ResponseEntity<?> getUserById(@PathVariable(name = "id") UUID id) {
 
     return new ResponseEntity<>(user, HttpStatus.OK);
 }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteUserById(@PathVariable(name = "id") UUID id) {
+	    try {
+	        userService.deleteById(id.toString());
+	        return new ResponseEntity<>(
+	                new MessageDTO("User deleted"),
+	                HttpStatus.OK);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return new ResponseEntity<>(
+	                new MessageDTO("Internal Server Error"),
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
 	
 	@GetMapping("/users")
 	public ResponseEntity<?> findAllUsers(){
